487c2e5e0809f488a02cd28b8d04bfecdf10d850
Vinicius Morais <vinidev.morais@gmail.com>
2024-03-05 14:11:11 -0300

    criado a classe Veiculo

---
+++ entities/Veiculo.cs
@@ -0,0 +1,12 @@
+namespace Atividade_Interfarce.entities
+{
+    public class Veiculo
+    {
+        public string Modelo { get; set; }
+
+        public Veiculo (string modelo)
+        {
+            Modelo = modelo;
+        }
+    }
+}
